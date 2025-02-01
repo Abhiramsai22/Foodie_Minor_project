73571b7ec360166fb799c04510d39e4936ae73e6
Abhiramsai22 <narayandaduabhiramsai@gmail.com>
2025-02-01 20:05:55 +0530

    p2

---
+++ python/index.py
@@ -0,0 +1,38 @@
+# for i in range(1,22,2):
+#     print(i,end=" ")
+
+# num1=4
+# while num1<5:
+#     print("kill.....")
+#     print("still fighting")
+#     num1+=1
+
+# n=5
+# while(n<26):
+#     if n%2 == 0:
+#         print(n,"even")
+#     else:
+#         print(n,"odd")
+#     n+=1
+
+# j=0
+# i=1
+# while(j<11):
+#     j+=1
+#     if j!=5 and j!=7:
+#         while(i<31):
+#             i += 1
+#             print(j,i)
+
+# for i in range(0,31):
+#     print(i)
+#     if (i==2 or i==5):
+#         break
+
+# for c in range(1,11):
+#     for r in range(1,30):
+#         if c == 3 and r>15:
+#             continue
+#         print()
+
+
